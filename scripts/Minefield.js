d2a6789828f8c0f5140f56d15ac75dfab41b88dc
Curtis Reynolds <pg20curtis@vfs.com>
2021-02-09 14:08:30 -0800

    Working event handlers on minefield
    Also figured out way to make right click work for flag placement

--- scripts/Minefield.js
+++ scripts/Minefield.js
@@ -10,12 +10,63 @@ export default class Minefield{
         this.size = size;
         this.field = []; // turn this into a 2d array of squares
 
+        
+        this.generateBoard();
+        this.updateCellHandlers();
         //initialize the minefield with empty squares
         this._init();
         //init minefield with n mines
         this._randomizeMines(mineCount);
         //tell all the squares to compute adjacent mines
         this._countAdjacent();
+    }
+
+    generateBoard(){
+        /*
+        <table>
+            <tr><td></td><td>...</td>.
+        </table>
+        */
+        
+        
+        let markup = "<table>";
+        for (let row = 0; row < this.size; row++) {
+
+            markup += "<tr>";
+            for(let col =0; col < this.size; col++){
+                
+                const id = `square-${row}-${col}`; // "square-5-4"
+                markup += `<td id="${id}" class="square"><div > </div></td>`;
+            }
+            markup += "</tr>";
+        }
+        markup += "</table>";
+        //find the game area, attach this table
+        document.querySelector("#game-grid").innerHTML = markup;
+    }
+
+    updateCellHandlers(){
+        //click the square
+        document.querySelectorAll(".square")
+            .forEach(element =>{
+                element.addEventListener('click', event =>{
+                        if(event.button === 0){
+                            element.classList.add("gird-clicked"); 
+                        }
+                })
+                element.addEventListener('contextmenu', event =>{
+                    if(event.button === 2){
+                        element.classList.add("gird-right-clicked"); 
+                    }
+                    event.preventDefault();
+                });
+            })
+                
+        //flag the square
+
+
+
+        //right click hold to show adjacent squares
     }
 
     _init()
--- scripts/game.js
+++ scripts/game.js
@@ -40,32 +40,8 @@ export default class Game {
         //change the dom and the screen to show the player what's going on
 
         //generate the playfield
-        this.generateBoard();
+
         //this.resize();
-    }
-
-    generateBoard(){
-        /*
-        <table>
-            <tr><td></td><td>...</td>.
-        </table>
-        */
-        
-        
-        let markup = "<table>";
-        for (let row = 0; row < SIZE; row++) {
-
-            markup += "<tr>";
-            for(let col =0; col < SIZE; col++){
-                
-                const id = `square-${row}-${col}`; // "square-5-4"
-                markup += `<td id="${id} class="unknown""><div > </div></td>`;
-            }
-            markup += "</tr>";
-        }
-        markup += "</table>";
-        //find the game area, attach this table
-        document.querySelector("#game-grid").innerHTML = markup;
     }
 
 
